1acd8539e1f64b15898847270848727419e7f642
Jonas Andersson <ja@kth.se>
2016-12-30 11:06:04 +0100

    security fix

--- app.py
+++ app.py
@@ -119,6 +119,7 @@ if __name__ == '__main__':
       ctxt.load_cert_chain("/etc/letsencrypt/live/%s/cert.pem" % h,"/etc/letsencrypt/live/%s/privkey.pem" % h)
       ctxt.options|=ssl.OP_NO_SSLv2
       ctxt.options|=ssl.OP_NO_SSLv3
+      ctxt.options|=ssl.OP_NO_TLSv1
       app.run(host='0.0.0.0',port=443,ssl_context=ctxt)
    except:
       app.run(host='0.0.0.0',port=443,ssl_context='adhoc')
