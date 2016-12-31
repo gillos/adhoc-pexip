788be9f10f55dd6f47ccced652ad1fc312f51f6b
Jonas Andersson <ja@kth.se>
2016-12-31 19:30:54 +0100

    fixed mail templating

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ def success():
       l=message.split(',')
       x=get("https://www.lan.kth.se/~ja/mime.html")
       msgx=jinja2.Template(x.text)
-      sendemail(l,"",msgx.render(from=SMTP_SENDER,to=",".join(l),meeting_url="https://%s/webapp/?conference=%s&pin=%s&join=1" % (PEXIP_URL,room,pin),access_code=session['alias']))
+      sendemail(l,"",msgx.render(sender=SMTP_SENDER,to=",".join(l),meeting_url="https://%s/webapp/?conference=%s&pin=%s&join=1" % (PEXIP_URL,room,pin),access_code=session['alias']))
       #sendemail(l,"https://%s/webapp/?conference=%s&pin=%s&join=1\naccess code: %s" % (PEXIP_URL,room,pin,session['alias']))
       return render_template('wait2.html',pin=pin,room=room,url=PEXIP_URL,name=name)
    else: 
